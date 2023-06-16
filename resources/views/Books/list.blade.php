d0cc99b4110ef72183385f9caf9b6f058ac3a1e2
gerges2 <gergesvictor512@gmail.com>
2023-06-16 19:56:58 +0300

    inil  crud operation for  books

---
+++ resources/views/Books/list.blade.php
@@ -0,0 +1,66 @@
+{{-- @extends('layouts.app') --}}
+{{-- @section('content') --}}
+<div class="container">
+<?php
+//  use App\Models\User; 
+?>    
+    @if (session('success'))
+    <div class ='alert alert-success'>
+        {{session('success')}}
+    </div>
+        @endif
+ 
+    <a class ='btn btn-primary'  href={{ route('books.create') }}>add new book </a>
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">title</th>
+            <th scope="col">description</th>
+            <th scope="col">Image</th>
+            <th scope="col">Auther_name</th>
+            <th scope="col">Category_name </th>
+            <th scope="col">created_date</th>
+            <th scope="col">actions</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($books as $book)
+            <tr>
+                    
+                <td>{{ $loop->iteration }}</td>
+                {{-- <td> {{  $student->user->name }}</td> --}}
+                {{-- <td> {{User::find(  $student->user_id)->name }}</td> --}}
+                {{-- <td>{{ $student->user_id }}</td> --}}
+                <td>{{ $book->title  }}</td>
+                <td>{{ $book->description }}</td>
+                <td><img height=100px width="100px" src="{{asset('/storage/images/books/'.$book->Image)}}"></td>
+                <td>{{ $book->Auther_id }}</td>
+                <td>{{ $book->Category_id }}</td>
+                <td>{{ $book->created_at }}</td>
+                
+
+                <td>
+                {{-- @can('update', $book) --}}
+                <a class ='btn btn-primary'  href={{ route('books.edit', "$book->id") }}>edit book </a>
+                {{-- @endcan --}}
+                </td>
+
+
+                <td>
+                {!! Form ::open(['route' => ['books.destroy',$book->id], 'method' => 'delete']) !!} 
+                <div class="mb-3">
+                    {!!   Form::submit('delete!' ,['class' => 'btn btn-danger'])  !!}    
+                </div>
+                {!! Form::close() !!}
+                </td>
+            </tr>
+        @endforeach
+        
+          
+        </tbody>
+      </table>
+
+  {{-- @endsection --}}
+    
