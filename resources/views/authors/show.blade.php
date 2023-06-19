565a1eefa524d64867c1016e4f9cfa3a903828df
Aya <aya.rashed.official@gmail.com>
2023-06-19 18:49:57 +0200

    view show page for authors

---
+++ resources/views/authors/show.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <h2>{{ $author->name }}</h2>
+    <p>{{ $author->date }}</p>
+    <p>{{ $author->brief }}</p>
+    <a href="{{ route('authors.edit', $author) }}">Edit</a>
+    <form action="{{ route('authors.destroy', $author) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="submit">Delete</button>
+    </form>
+</div>
