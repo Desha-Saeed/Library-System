ef63f802feb80a11e8dda65a4a1b3d95f78a853a
Khaled-318 <khaledd.adel318@gmail.com>
2023-06-20 15:48:08 +0300

    Add category crud operations, validation and relation

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('categories', App\Http\Controllers\CategoryController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
